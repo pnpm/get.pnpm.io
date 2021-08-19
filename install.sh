bc2e8a0507d23e60d0938ed171410f3d3636d433
Kaito Udagawa <umireon@gmail.com>
2021-08-19 23:30:40 +0900

    remove tty_underline

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ else
   tty_escape() { :; }
 fi
 tty_mkbold() { tty_escape "1;$1"; }
-tty_underline="$(tty_escape "4;39")"
 tty_blue="$(tty_mkbold 34)"
 tty_red="$(tty_mkbold 31)"
 tty_bold="$(tty_mkbold 39)"
