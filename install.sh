9648b4fb86aaff6ff77de5bb55952447f00ed24b
Kaito Udagawa <umireon@gmail.com>
2021-08-20 02:10:14 +0900

    fancy logging (#7)
    * fancy logging
    
    * lint
    
    * add newline

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
-info() {
-  echo "$@"
-}
-
-error() {
-  echo "$@"
+# From https://github.com/Homebrew/install/blob/master/install.sh
+abort() {
+  printf "%s\n" "$@"
   exit 1
 }
+
+# string formatters
+if [ -t 1 ]; then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+tty_mkbold() { tty_escape "1;$1"; }
+tty_blue="$(tty_mkbold 34)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+
+ohai() {
+  printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$1"
+}
+
+# End from https://github.com/Homebrew/install/blob/master/install.sh
 
 detect_platform() {
   local platform
@@ -42,12 +56,14 @@ detect_arch() {
 
   case "$arch" in
     x64*) ;;
-    *) error "Sorry! pnpm currently only provides pre-built binaries for x86_64 architectures."
+    *) abort "Sorry! pnpm currently only provides pre-built binaries for x86_64 architectures."
   esac
   printf '%s' "${arch}"
 }
 
 detect_arch
+echo
+
 platform="$(detect_platform)"
 arch="$(detect_arch)"
 pkgName="@pnpm/${platform}-${arch}"
@@ -57,27 +73,23 @@ archive_url="https://registry.npmjs.org/${pkgName}/-/${platform}-${arch}-${versi
 curl --progress-bar --show-error --location --output "pnpm.tgz" "$archive_url"
 
 create_tree() {
-  local tmp_dir
-  tmp_dir="$1"
+  local tmp_dir="$1"
 
-  info 'Creating' "directory layout"
+  ohai 'Creating directory layout'
 
   if ! mkdir -p "$tmp_dir";
   then
-    error "Could not create directory layout. Please make sure the target directory is writeable: $tmp_dir"
-    exit 1
+    abort "Could not create directory layout. Please make sure the target directory is writeable: $tmp_dir"
   fi
 }
 
 install_from_file() {
-  local archive
-  local tmp_dir
-  archive="$1"
-  tmp_dir="$2"
+  local archive="$1"
+  local tmp_dir="$2"
 
   create_tree "$tmp_dir"
 
-  info 'Extracting' "pnpm binaries"
+  ohai 'Extracting pnpm binaries'
   # extract the files to the specified directory
   tar -xf "$archive" -C "$tmp_dir" --strip-components=1
   SHELL=$SHELL "$tmp_dir/pnpm" setup
