ea089de731b440b0a4412237ec8784187907645a
Zoltan Kochan <z@kochan.io>
2021-08-23 22:14:13 +0300

    chore: update SHASUMS

--- _update.sh
+++ _update.sh
@@ -3,7 +3,6 @@ sha256sum v6.js >> SHASUMS256.txt
 sha256sum v6.6.js >> SHASUMS256.txt
 sha256sum v6.7.js >> SHASUMS256.txt
 sha256sum v6.14.js >> SHASUMS256.txt
-sha256sum v6.14.js >> SHASUMS256.txt
 sha256sum install.sh >> SHASUMS256.txt
 gpg --local-user pnpm --detach-sign SHASUMS256.txt
 
