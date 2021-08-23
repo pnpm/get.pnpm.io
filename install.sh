2dfac9993ce9d7af9eb0b2c91088246b6fef2ae1
Kaito Udagawa <umireon@gmail.com>
2021-08-24 00:24:43 +0900

    fix indent

--- install.sh
+++ install.sh
@@ -24,11 +24,11 @@ ohai() {
 # End from https://github.com/Homebrew/install/blob/master/install.sh
 
 download() {
-    if command -v curl > /dev/null 2>&1; then
-        curl -fsSL "$1"
-    else
-        wget -qO- "$1"
-    fi
+  if command -v curl > /dev/null 2>&1; then
+    curl -fsSL "$1"
+  else
+    wget -qO- "$1"
+  fi
 }
 
 detect_platform() {
