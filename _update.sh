0c8bfc2eed1110d4bfb7ab00f50d9f5d21d8c123
Zoltan Kochan <z@kochan.io>
2021-08-23 22:17:07 +0300

    style: fix _update.sh

--- _update.sh
+++ _update.sh
@@ -1,8 +1,10 @@
 rm SHASUMS256.txt SHASUMS256.txt.sig
-sha256sum v6.js >> SHASUMS256.txt
-sha256sum v6.6.js >> SHASUMS256.txt
-sha256sum v6.7.js >> SHASUMS256.txt
-sha256sum v6.14.js >> SHASUMS256.txt
-sha256sum install.sh >> SHASUMS256.txt
+{
+  sha256sum v6.js;
+  sha256sum v6.6.js;
+  sha256sum v6.7.js;
+  sha256sum v6.14.js;
+  sha256sum install.sh;
+} >> SHASUMS256.txt
 gpg --local-user pnpm --detach-sign SHASUMS256.txt
 
