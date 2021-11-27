497ce5e65041f3ed692af6792f4bfbcf272a8af0
Zoltan Kochan <z@kochan.io>
2021-11-27 21:57:09 +0200

    update the checksums

--- _update.sh
+++ _update.sh
@@ -6,6 +6,7 @@ rm SHASUMS256.txt SHASUMS256.txt.sig
   sha256sum v6.14.js;
   sha256sum v6.16.js;
   sha256sum install.sh;
+  sha256sum install.ps1;
 } >> SHASUMS256.txt
 gpg --local-user pnpm --detach-sign SHASUMS256.txt
 
