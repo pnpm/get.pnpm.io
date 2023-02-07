2646a6c64aecb5bd5377ffab32c0ed7c64a473cb
Zoltan Kochan <z@kochan.io>
2023-02-08 00:13:49 +0200

    ci: verify integrity

---
+++ _check.sh
@@ -0,0 +1,10 @@
+grep v6.js SHASUMS256.txt | sha256sum -c -
+grep v6.6.js SHASUMS256.txt | sha256sum -c -
+grep v6.7.js SHASUMS256.txt | sha256sum -c -
+grep v6.14.js SHASUMS256.txt | sha256sum -c -
+grep v6.16.js SHASUMS256.txt | sha256sum -c -
+grep v6.32.js SHASUMS256.txt | sha256sum -c -
+grep install.sh SHASUMS256.txt | sha256sum -c -
+grep install.ps1 SHASUMS256.txt | sha256sum -c -
+curl https://keybase.io/pnpm/pgp_keys.asc | gpg --import
+gpg --verify SHASUMS256.txt.sig SHASUMS256.txt
